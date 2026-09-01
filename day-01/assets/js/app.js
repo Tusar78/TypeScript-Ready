3ad657c073ef3e0eec5505e12b12f6947d9d9b69
Tusar78 <tusarahmed141@gmail.com>
2026-09-02 05:47:54 +0600

    Solve some normal problem

--- day-01/assets/js/app.js
+++ day-01/assets/js/app.js
@@ -65,21 +65,51 @@
 // const flower = ['Rose', 'Tulip', 'Lily', 'Daisy', 'Sunflower'];
 // const squareNumbers = applyToString(flower, (n) => n + ' is a beautiful flower');
 // console.log(squareNumbers);
-let skills = ['JavaScript', 'TypeScript', 'React', 'Node.js'];
-let scores = [85, 90, 95, 80];
-let anotherSkills = ['JavaScript', 'TypeScript', 'React', 'Node.js'];
-let userInfo = ['Tusar', 123456789, 'Jannat'];
-console.log(userInfo);
-const getAverageScore = (scores) => {
-    const total = scores.reduce((acc, score) => acc + score, 0);
-    return total / scores.length;
+// let skills: string[] = ['JavaScript', 'TypeScript', 'React', 'Node.js'];
+// let scores: number[] = [85, 90, 95, 80];
+// let anotherSkills: Array<string> = ['JavaScript', 'TypeScript', 'React', 'Node.js'];
+// let userInfo: [string, number, string] = ['Tusar', 123456789, 'Jannat'];
+// console.log(userInfo);
+// const getAverageScore = (scores: number[]): number => {
+//     const total = scores.reduce((acc, score) => acc + score, 0);
+//     return total / scores.length;
+// }
+// const averageScore = getAverageScore(scores);
+// console.log(`Average Score: ${averageScore}`);
+// const useToggle = (initial : number): [number, () => void] => {
+//     let state = initial;
+//     const toggle = () => state = ++state;
+//     return [ state, toggle];
+// }
+// const [result, toggle] = useToggle(10);
+// console.log(toggle());
+// const num: [number, number] = [10, 20];
+// num.push(30); // This is allowed because push does not violate the tuple's fixed length
+// console.log(num.length);
+// Problem One
+const getTopSkills = (skills) => {
+    let bigString = '';
+    skills.map(skill => {
+        if (bigString.length < skill.length) {
+            bigString = skill;
+        }
+    });
+    return bigString;
 };
-const averageScore = getAverageScore(scores);
-console.log(`Average Score: ${averageScore}`);
-const useToggle = (initial) => {
-    let state = initial;
-    const toggle = () => state = ++state;
-    return [state, toggle];
+let skills = ['JavaScript', 'Next.js', 'React', 'Node.js', 'Python', 'C++'];
+const topSkills = getTopSkills(skills);
+console.log(topSkills);
+// Problem Two
+const createCordinate = (x, y) => {
+    return [x, y];
 };
-const [result, toggle] = useToggle(10);
-console.log(toggle());
+const cordinate = createCordinate(10, 20);
+console.log(cordinate);
+// Problem Three
+const parseFullName = (fullName) => {
+    const [firstName, lastName] = fullName.split(' ');
+    return [firstName, lastName];
+};
+const fullName = "Tusar Ali";
+const [firstName, lastName] = parseFullName(fullName);
+console.log(`First Name: ${firstName}, Last Name: ${lastName}`);
--- day-01/assets/ts/app.ts
+++ day-01/assets/ts/app.ts
@@ -86,29 +86,70 @@
 // console.log(squareNumbers);
 
 
-let skills: string[] = ['JavaScript', 'TypeScript', 'React', 'Node.js'];
-let scores: number[] = [85, 90, 95, 80];
+// let skills: string[] = ['JavaScript', 'TypeScript', 'React', 'Node.js'];
+// let scores: number[] = [85, 90, 95, 80];
 
-let anotherSkills: Array<string> = ['JavaScript', 'TypeScript', 'React', 'Node.js'];
+// let anotherSkills: Array<string> = ['JavaScript', 'TypeScript', 'React', 'Node.js'];
 
-let userInfo: [string, number, string] = ['Tusar', 123456789, 'Jannat'];
-console.log(userInfo);
+// let userInfo: [string, number, string] = ['Tusar', 123456789, 'Jannat'];
+// console.log(userInfo);
 
 
 
-const getAverageScore = (scores: number[]): number => {
-    const total = scores.reduce((acc, score) => acc + score, 0);
-    return total / scores.length;
+// const getAverageScore = (scores: number[]): number => {
+//     const total = scores.reduce((acc, score) => acc + score, 0);
+//     return total / scores.length;
+// }
+
+// const averageScore = getAverageScore(scores);
+// console.log(`Average Score: ${averageScore}`);
+
+// const useToggle = (initial : number): [number, () => void] => {
+//     let state = initial;
+//     const toggle = () => state = ++state;
+//     return [ state, toggle];
+// }
+
+// const [result, toggle] = useToggle(10);
+// console.log(toggle());
+
+
+// const num: [number, number] = [10, 20];
+// num.push(30); // This is allowed because push does not violate the tuple's fixed length
+// console.log(num.length);
+
+
+
+// Problem One
+const getTopSkills = (skills: string[]): string => {
+    let bigString: string = '';
+    skills.map(skill => {
+        if (bigString.length < skill.length) {
+            bigString = skill;
+        }
+    });
+    return bigString;
 }
 
-const averageScore = getAverageScore(scores);
-console.log(`Average Score: ${averageScore}`);
+let skills: string[] = ['JavaScript', 'Next.js', 'React', 'Node.js', 'Python', 'C++'];
+const topSkills = getTopSkills(skills);
+console.log(topSkills); 
 
-const useToggle = (initial : number): [number, () => void] => {
-    let state = initial;
-    const toggle = () => state = ++state;
-    return [ state, toggle];
+
+// Problem Two
+const createCordinate = (x: number, y: number): [number, number] => {
+    return [x, y];
 }
 
-const [result, toggle] = useToggle(10);
-console.log(toggle());
+const cordinate = createCordinate(10, 20);
+console.log(cordinate); 
+
+// Problem Three
+const parseFullName = (fullName: string): [string, string] => {
+    const [firstName, lastName] = fullName.split(' ');
+    return [firstName, lastName];
+}
+
+const fullName = "Tusar Ali";
+const [firstName, lastName] = parseFullName(fullName);
+console.log(`First Name: ${firstName}, Last Name: ${lastName}`);
